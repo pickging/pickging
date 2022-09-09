d2ceccf876c30ab01f2842545243c4ad83b5ab51
kjy1101 <amelia1023@naver.com>
2022-09-10 04:33:21 +0900

    comment

--- map/urls.py
+++ map/urls.py
@@ -3,8 +3,8 @@ from .views import *
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('path', PathViewSet) # 경로
-router.register('spot', SpotViewSet) # 스팟
+router.register('path', PathViewSet) # 경로 -> 127.0.0.1:8000/api/map/path/<km수> -> 1,3,5km 경로 반환
+router.register('spot', SpotViewSet) # 스팟 -> 127.0.0.1:8000/api/map/spot -> 모든 스팟 반환
 
 # api/map/
 urlpatterns = [
