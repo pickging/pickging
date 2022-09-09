a829a43840c446a85a8a707376eaea3d49ade6b8
Jiwoo An <jiyouan9@gmail.com>
2022-09-10 02:51:25 +0900

    chore: 필요 없는 부분 삭제 & 주석 추가

--- guild/views.py
+++ guild/views.py
@@ -5,26 +5,23 @@ from django.http import Http404
 from .models import *
 from .serializers import *
 
+
 # Create your views here.
+"""
+길드 CRUD
+path: /api/guild/guild/
+
+"""
 class GuildViewSet(ModelViewSet):
-    print("왜 여기ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ ======")
     serializer_class = GuildSerializer
     queryset = Guild.objects.all()
-    def create(self, request):
-        print("여기는?ㅁㄹ안라임")
-        
-        return Response({"guild": request.user})
-    
-    def delete(self, request, guild_id):
-        print("왜 여기ㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣㅣ ======", request.user)
-        guild = Guild.objects.filter(id=guild_id).first()
-        print("guild =====", guild.member_number)
-        guild.member_number = guild.member_number - 1
-        print("after guild =====", guild.member_number)
 
-        guild.save()
 
-# GET, DELETE /api/guild/inout/<guild_id>
+"""
+길드 가입(GET), 탈퇴(DELETE)
+path: /api/guild/inout/:guild_id/
+
+"""
 class InOutGuild(APIView):
     def get(self, request, guild_id):
         guild_list = Guild.objects.all().order_by('-member_number')
@@ -37,7 +34,7 @@ class InOutGuild(APIView):
             serializer_context = {'request': request,}
             serializer_class = GuildSerializer(guild_list, many=True, context=serializer_context)
             #! 유저 serialize 같이 보내주기
-            return Response({"guild": serializer_class.data})
+            return Response({"guild_list": serializer_class.data})
         else:
             raise Http404("길드 존재하지 않음~!~")
         
@@ -52,7 +49,7 @@ class InOutGuild(APIView):
             serializer_context = {'request': request,}
             serializer_class = GuildSerializer(guild_list, many=True, context=serializer_context)
             #! 유저 serialize 같이 보내주기
-            return Response({"guild": serializer_class.data})
+            return Response({"guild_list": serializer_class.data})
         else:
             raise Http404("길드 존재하지 않음~!~")
     
