a79bd3577957941c83661622af42366e18639fe3
Jiwoo An <jiyouan9@gmail.com>
2022-09-11 02:56:28 +0900

    modify: 가입된 길드 거절하기

--- guild/views.py
+++ guild/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from .models import *
 from .serializers import *
 from rest_framework.permissions import IsAuthenticated
+from django.http import JsonResponse
+from rest_framework import status
 
 from user.models import User
 from user.serializers import UserSerializer
@@ -30,11 +32,21 @@ class InOutGuild(APIView):
         permission_classes = [IsAuthenticated]
         guild_list = Guild.objects.all().order_by('-member_number')
         guild = Guild.objects.filter(pk=guild_id).first()
+        user = User.objects.filter(email=request.user).first()
+        if user.guild.id == guild_id:
+            return JsonResponse({'err_msg': '이미 길드의 회원임!!'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+)
         if guild:
-            user = User.objects.filter(email=request.user).first()
-            user.guild = guild
+            if user.guild: # 가입한 길드가 있다면
+                delete_guild = Guild.objects.filter(pk=user.guild.id).first()
+                delete_guild.member_number = delete_guild.member_number - 1
+                delete_guild.save()
+                user.guild = guild
+                guild.member_number = guild.member_number + 1
+            else:
+                user.guild = guild
+                guild.member_number = guild.member_number + 1
             user.save()
-            guild.member_number = guild.member_number + 1
             guild.save()
             serializer_context = {'request': request,}
             serializer_class = GuildSerializer(guild_list, many=True, context=serializer_context)
