173d4585b8e70d42ba3295f002ff5080a06190d5
Jiwoo An <jiyouan9@gmail.com>
2022-09-14 21:35:47 +0900

    feat: user 모델에 nickname 추가

--- user/admin.py
+++ user/admin.py
@@ -3,6 +3,6 @@ from .models import *
 
 # Register your models here.
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'email', 'guild', 'region', 'point', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'created_at', 'updated_at', 'password']
+    list_display = ['id', 'email', 'nickname', 'guild', 'region', 'point', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'created_at', 'updated_at', 'password']
 
 admin.site.register(User, UserAdmin)
