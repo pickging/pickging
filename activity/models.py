b4caeadb1a359a0859b740f4eed41c968ea5da87
Jiwoo An <jiyouan9@gmail.com>
2022-09-14 18:56:53 +0900

    feat: 심박수 추가

--- activity/models.py
+++ activity/models.py
@@ -7,5 +7,6 @@ class Activity(models.Model):
     date = models.DateField(auto_now_add=True, help_text="날짜")
     point = models.IntegerField(default=0, help_text="포인트")
     garbage = models.FloatField(default=0, help_text="쓰레기양")
+    heart_rate = models.IntegerField(default=0, help_text="심박수")
     step_count = models.IntegerField(default=0, help_text="걸음수")
     review = models.IntegerField(default=0, help_text="별점")
