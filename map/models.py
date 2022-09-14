927a129712c42ffbcafc07f964296402aab7a3d7
Jaeyeon Kim <amelia1023@naver.com>
2022-09-14 11:07:00 +0900

    path geojson update

--- map/models.py
+++ map/models.py
@@ -1,6 +1,6 @@
 from random import choices
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+# from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Path(models.Model):
@@ -8,10 +8,10 @@ class Path(models.Model):
         km2 = 2    # 2km
         km3 = 3    # 3km
         km5 = 5    # 5km
-        km7 = 7    # 7km
 
-    length_category = models.IntegerField(choices=PathLength.choices, help_text="경로 길이")
-    path_coordinate = ArrayField(models.CharField(max_length=30), null=True, blank=True, help_text="경로 위경도 좌표 배열") # ['127.0 35.0', '127.1 35.1']
+    length_category = models.IntegerField(choices=PathLength.choices, help_text="경로 거리 범주")
+    # path_coordinate = ArrayField(models.CharField(max_length=30), null=True, blank=True, help_text="경로 위경도 좌표 배열") # ['127.0 35.0', '127.1 35.1']
+    path_geojson = models.TextField(null=True, blank=True, help_text="경로 geojson (문자열)")
     path_length = models.FloatField(default=0, help_text="실제 경로 거리")
     maybe_time = models.IntegerField(default=0, help_text="예상 소요시간(분)")
     mission = models.CharField(max_length=200, null=True, blank=True, help_text="미션")
@@ -24,5 +24,6 @@ class Spot(models.Model):
         SOL_SPOT = 0    # 솔방울 정산소
         PICK_SPOT = 1   # 픽스팟
 
-    spot_coordinate = ArrayField(models.FloatField(), null=True, blank=True, help_text="스팟 위경도") # [127.1, 35.0]
+    # spot_coordinate = ArrayField(models.FloatField(), null=True, blank=True, help_text="스팟 위경도") # [127.1, 35.0]
+    spot_geojson = models.TextField(null=True, blank=True, help_text="스팟 geojson (문자열)")
     spot_type = models.IntegerField(choices=SpotType.choices, help_text="스팟 종류(0:솔방울정산소, 1:픽스팟)")
