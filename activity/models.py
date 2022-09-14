e9dc520b33fb209ad98cf4c7d16be79e9ba0acdb
Jiwoo An <jiyouan9@gmail.com>
2022-09-14 20:34:05 +0900

    feat: path_name 같이 보내기

--- activity/models.py
+++ activity/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Activity(models.Model):
-    user = models.ForeignKey("user.User", related_name="user", on_delete=models.CASCADE, null=True, blank=True, help_text="유저")
-    path = models.ForeignKey("map.Path", related_name="path", on_delete=models.CASCADE, null=True, blank=True, help_text="경로")
+    user_id = models.ForeignKey("user.User", related_name="user", on_delete=models.CASCADE, null=True, blank=True, help_text="유저")
+    path_id = models.ForeignKey("map.Path", related_name="path", on_delete=models.CASCADE, null=True, blank=True, help_text="경로")
     date = models.DateField(auto_now_add=True, help_text="날짜")
     point = models.IntegerField(default=0, help_text="포인트")
     garbage = models.FloatField(default=0, help_text="쓰레기양")
