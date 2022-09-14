5076ffdbd233da12f7cea41cb9ff73a9af9c5d64
Jaeyeon Kim <amelia1023@naver.com>
2022-09-14 14:14:37 +0900

    add path & spot name

--- map/models.py
+++ map/models.py
@@ -8,19 +8,24 @@ class Path(models.Model):
         km3 = 3    # 3km
         km5 = 5    # 5km
 
+    name = models.CharField(max_length=20, help_text="경로 이름")
     length_category = models.IntegerField(choices=PathLength.choices, help_text="경로 거리 범주")
-    path_geojson = models.TextField(null=True, blank=True, help_text="경로 geojson (문자열)")
-    path_length = models.FloatField(default=0, help_text="실제 경로 거리")
+    geojson = models.TextField(null=True, blank=True, help_text="경로 geojson (문자열)")
+    length = models.FloatField(default=0, help_text="실제 경로 거리")
     maybe_time = models.IntegerField(default=0, help_text="예상 소요시간(분)")
     mission = models.CharField(max_length=200, null=True, blank=True, help_text="미션")
 
     def __str__(self):
-        return str(self.length_category) + "km 경로"
+        return str(self.name)
 
 class Spot(models.Model):
     class SpotType(models.IntegerChoices):
         SOL_SPOT = 0    # 솔방울 정산소
         PICK_SPOT = 1   # 픽스팟
 
-    spot_geojson = models.TextField(null=True, blank=True, help_text="스팟 geojson (문자열)")
-    spot_type = models.IntegerField(choices=SpotType.choices, help_text="스팟 종류(0:솔방울정산소, 1:픽스팟)")
+    name = models.CharField(max_length=20, help_text="스팟 이름")
+    geojson = models.TextField(null=True, blank=True, help_text="스팟 geojson (문자열)")
+    type = models.IntegerField(choices=SpotType.choices, help_text="스팟 종류(0:솔방울정산소, 1:픽스팟)")
+
+    def __str__(self):
+        return str(self.name)
